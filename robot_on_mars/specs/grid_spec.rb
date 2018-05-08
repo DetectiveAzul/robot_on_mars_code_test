07399ffa1d2c5ac3be242c4c7c7e34376c27e1ff
Jaime Lopez <detec.azul@gmail.com>
2018-05-08 21:45:10 +0100

    Basic Grid Test written

---
+++ robot_on_mars/specs/grid_spec.rb
@@ -0,0 +1,27 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../model/grid')
+
+class GridTest < MiniTest::Test
+  def setup()
+    @mars = Grid.new(30, 20)
+
+  end
+
+  def test_has_top_x()
+    assert_equal(30, @mars.get_top_x)
+  end
+
+  def test_has_top_y()
+    assert_equal(20, @mars.get_top_y)
+  end
+
+  def test_has_robot_array()
+    assert_equal(0, @mars.robots.size)
+  end
+
+  def test_has_scent_array()
+    assert_equal(0, @mars.scents.size)
+  end
+
+end
