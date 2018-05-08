9b53943c03ce8a6c72cefd3b6b204ba7dbe55e88
Jaime Lopez <detec.azul@gmail.com>
2018-05-08 23:14:45 +0100

    Computer get user input

--- robot_on_mars/model/computer.rb
+++ robot_on_mars/model/computer.rb
@@ -2,10 +2,11 @@ require_relative('grid')
 require_relative('robot')
 
 class Computer
-  attr_accessor :grid, :robots
+  attr_accessor :grid, :robots, :outputs
 
   def initialize()
     @robots = Array.new
+    @outputs = Array.new
   end
 
   def create_grid(top_x, top_y)
@@ -21,7 +22,22 @@ class Computer
   def execute_robots_instructions()
     @robots.each do |robot|
       robot.execute_instructions(@grid)
+      @output << robot.get_output
     end
   end
 
+  def get_user_input()
+    user_input = gets.chomp
+    user_input = user.input.split(" ")
+  end
+
+  def get_user_input_for_grid()
+    user_input = get_user_input()
+    create_grid(user_input[0], user_input[1])
+  end
+
+  def get_user_input_for_robot()
+    user_input = get_user_input()
+    send_robot([user_input[0], user_input[1]], user_input[2], user_input[3])
+  end
 end
